07309f0c44dd7d1c130a6e5dcf17a0722d4272cd
Tom Vanasse <thomas.j.vanasse@gmail.com>
2018-01-18 12:56:38 -0600

    Commit 2
    Script Cleaning

--- heatmap_dendro_clustering.R
+++ heatmap_dendro_clustering.R
@@ -1,7 +1,18 @@
-setwd("/Users/thomasvanasse/Google Drive/RESEARCH/METHODS/Methods_work/SHARED_DATA/HCA/")
+# The code below reproduces clustering analyses and figures conducted in the manuscript "BrainMapVBM: An Environment for Structural Meta-analysis" 
+# by Vanasse et al. (Figure 5, Sup. Figure 6, Sup. Figure 7) For details regarding the motivation behind analyses and the interpretation of results, 
+# see the manuscript.
+#
+# written by Thomas Vanasse (thomas.j.vanasse@gmail.com), June 2016 - November 2017
 
 library(gplots)
 library(cluster)
+library(dendextend)
+library(RColorBrewer)
+library(gplots)
+library(cluster)
+
+
+setwd("/Users/thomasvanasse/Google Drive/RESEARCH/METHODS/Methods_work/SHARED_DATA/HCA/")
 
 #upload matrix of network-disease lodings (21 networks)
 mat <- data.matrix(read.csv("data_mat.csv", header = FALSE, sep = ","))
@@ -43,17 +54,11 @@ max(mat)
 min(mat)
 colors<- c(seq(-0.05, 0.024, length.out=40),seq(0.025, 0.18,length.out=80))
 
-# Get the dendextend package
-if(!require(dendextend)) install.packages("dendextend")
-library(dendextend)
-# get some colors
-library(RColorBrewer)
-
+#set color pallette
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
 #Gap Statistic
-library(cluster)
 par(mfrow=c(1,2))
 #Component
 mydist <- function(x) as.dist((1-cor(t(x))))
@@ -64,7 +69,8 @@ myclusGap <- clusGap(t(mat),
                      B = 100)
 plot(myclusGap, main="Component Gap Statistic", ylim=c(-0.10,0.30))
 abline(v=c(9), lty=2)
-#Disease
+
+#Disease Gap Statistic
 mydist <- function(x) as.dist((1-cor(t(x))))
 mycluster <- function(x, k) list(cluster=cutree(hclust(mydist(x), method = "average"),k=k))
 myclusGap <- clusGap(mat,
@@ -119,21 +125,13 @@ dend1 <- color_branches(dend1, k =11, col = col_vector)
 # Set colors for k branches (columns)
 dend2 <- color_branches(dend2, k = 9, col = col_vector)
 
-
-
-source("https://raw.githubusercontent.com/talgalili/dendextend/master/R/attr_access.R")
+# set colors of "branches" in dendrogram
 col_labels <- get_leaves_branches_col(dend1)
 column_col_labels <- get_leaves_branches_col(dend2)
-# But due to the way heatmap.2 works - we need to fix it to be in the 
-# order of the data!    
 col_labels <- col_labels[order(order.dendrogram(dend1))]
 column_col_labels <- column_col_labels[order(order.dendrogram(dend2))]
 
-
-# Creating Heat Map
-if(!require(gplots)) install.packages("gplots")
-library(gplots)
-
+#plot heatmap
 heatmap.2(mat, 
           Rowv=dend1,
           Colv=dend2,
@@ -143,12 +141,10 @@ heatmap.2(mat,
           margins=c(5,18), 
           trace="none", 
           density.info='histogram', 
-          #na.rm=TRUE,
           RowSideColors=col_labels,
           ColSideColors=column_col_labels,
           cexRow=0.98, #font: cex = 0.2 + 1/log10(nr)
           cexCol=1.3) # to add nice colored strips        
-#colRow = col_labels # to add nice colored labels - only for qplots 2.17.0 and higher
 
 #remove graphic
 dev.off()
